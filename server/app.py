9b22f88d5039bab52dda34f209ff9304d2a125e3
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-06-16 22:20:37 +0300

    set up project structure with config

--- server/app.py
+++ server/app.py
@@ -13,14 +13,6 @@ def create_app():
     db.init_app(app)
     Migrate(app, db)
 
-    from server.controllers.restaurant_controller import restaurant_bp
-    from server.controllers.pizza_controller import pizza_bp
-    from server.controllers.restaurant_pizza_controller import restaurant_pizza_bp
-
-    app.register_blueprint(restaurant_bp)
-    app.register_blueprint(pizza_bp)
-    app.register_blueprint(restaurant_pizza_bp)
-
     return app
 
 app = create_app()
