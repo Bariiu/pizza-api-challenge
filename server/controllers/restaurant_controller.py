43846aa8239b5e543b49e1055487068ed2465543
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-06-16 22:29:04 +0300

    added routes for listing

---
+++ server/controllers/restaurant_controller.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, jsonify, request
+from server.models.restaurant import Restaurant
+from server.config import db
+
+restaurant_bp = Blueprint('restaurant_bp', __name__, url_prefix='/restaurants')
+
+@restaurant_bp.route('/', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return jsonify([{
+        'id': r.id,
+        'name': r.name,
+        'address': r.address
+    } for r in restaurants])
+
+@restaurant_bp.route('/<int:id>', methods=['GET'])
+def get_restaurant(id):
+    restaurant = Restaurant.query.get_or_404(id)
+    return jsonify({
+        'id': restaurant.id,
+        'name': restaurant.name,
+        'address': restaurant.address,
+        'pizzas': [{
+            'id': p.id,
+            'name': p.name,
+            'ingredients': p.ingredients
+        } for p in restaurant.pizzas]
+    })
